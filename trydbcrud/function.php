a669828792c57f9efe211469cc2c00fd246b2968
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-03-23 23:39:25 +0800

    login function

---
+++ trydbcrud/function.php
@@ -0,0 +1,17 @@
+<?php
+function check_login($con) {
+    if (isset($_SESSION['id'])) {
+        $id = $_SESSION['id'];
+        $query = "select * from clients where id= '$id' limit 1";
+
+        $result = mysqli_query($con, $query);
+        if ($result &&  mysqli_num_rows($result) > 0) {
+            $user_data =  mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    }
+
+    //redirect to login
+    header('location: index.php');
+    die;
+}
