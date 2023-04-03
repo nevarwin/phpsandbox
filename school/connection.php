799bc6d54fc24551a99c3981e970c6893255144f
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-03 23:30:33 +0800

    deleted

--- school/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "123";
-$dbname = "loginsample";
-
-if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
-  die("connection failed!");
-}
