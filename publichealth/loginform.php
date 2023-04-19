86d025e5aeab954bd32f7f7a7b8e091d2424ed65
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-19 23:30:39 +0800

    added session then redesigned the orm

--- publichealth/loginform.php
+++ publichealth/loginform.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("connection.php");
-// include("function.php");
+include("function.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
@@ -15,8 +15,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($result && mysqli_num_rows($result) > 0) {
                 $user_data = mysqli_fetch_assoc($result);
                 if ($user_data['password'] === $password) {
+                    $_SESSION['logged_in'] = true;
                     $_SESSION['id'] = $user_data['id'];
-                    header('location: index.php');
+                    header('location: admin.php');
                     die;
                 }
             }
@@ -36,7 +37,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 
 <head>
-
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -44,37 +44,143 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- CSS File -->
-    <link rel="stylesheet" href="loginform.css">
+    <style>
+        *,
+        *:before,
+        *:after {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #EFF3EB;
+        }
+
+        .background {
+            width: 430px;
+            height: 520px;
+            position: absolute;
+            transform: translate(-50%, -50%);
+            left: 50%;
+            top: 50%;
+        }
+
+        .background .shape {
+            height: 200px;
+            width: 200px;
+            position: absolute;
+            border-radius: 50%;
+        }
+
+        .shape:first-child {
+            background: linear-gradient(#00ff87,
+                    #60efff);
+            left: -80px;
+            top: -80px;
+        }
+
+        .shape:last-child {
+            background: linear-gradient(to right,
+                    #0061ff,
+                    #60efff);
+            right: -30px;
+            bottom: -80px;
+        }
+
+        form {
+            height: 520px;
+            width: 400px;
+            background-color: rgba(255, 255, 255, 0.13);
+            position: absolute;
+            transform: translate(-50%, -50%);
+            top: 50%;
+            left: 50%;
+            border-radius: 10px;
+            backdrop-filter: blur(10px);
+            border: 2px solid rgba(255, 255, 255, 0.1);
+            box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+            padding: 50px 35px;
+        }
+
+        form * {
+            font-family: 'Poppins', sans-serif;
+            color: #080710;
+            letter-spacing: 0.5px;
+            outline: none;
+            border: none;
+        }
+
+        form h3 {
+            font-size: 32px;
+            font-weight: 500;
+            line-height: 42px;
+            text-align: center;
+            color: #697c56;
+        }
+
+        label {
+            display: block;
+            margin-top: 30px;
+            font-size: 16px;
+            font-weight: 500;
+            color: #697c56;
+
+        }
+
+        input {
+            display: block;
+            height: 50px;
+            width: 100%;
+            background-color: rgba(255, 255, 255, 0.07);
+            border-radius: 3px;
+            padding: 0 10px;
+            margin-top: 8px;
+            font-size: 14px;
+            font-weight: 300;
+            background-color: #e5e5e5;
+        }
+
+        ::placeholder {
+            color: black;
+
+
+        }
+
+        button {
+            margin-top: 50px;
+            width: 100%;
+            background-color: #ffffff;
+            color: #697c56;
+            padding: 15px 0;
+            font-size: 18px;
+            font-weight: 600;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+
 
 </head>
 
 <body>
-    <div class="login">
-        <h1 class="text-center">Welcome!</h1>
-        <form class="needs-validation" method="POST" name="loginForm">
-            <div class="form-group needs-validation">
-                <label class="form-label" for="email">Email address</label>
-                <input class="form-control" name="email" type="email" id="email" required>
-                <div class="invalid-feedback">
-                    Please enter your email address
-                </div>
-            </div>
-            <div class="form-group needs-validation">
-                <label class="form-label" for="password">Password</label>
-                <input class="form-control" name="password" type="password" id="password" required>
-                <div class="invalid-feedback">
-                    Please enter your password
-                </div>
-            </div>
-            <input class="btn btn-success w-100" type="submit" value="SIGN IN" onclick="checkForm(document.loginForm.password)">
-
-
-            <a href="./landingpage/landingpage.php" class="btn w-100">Cancel</a>
-        </form>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
     </div>
+    <form action="" method="post">
+        <h3>Login Here</h3>
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email" id="email" name="email">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" name="password">
+        <button>Log In</button>
+
+    </form>
+
 </body>
-<script>
+<!-- <script>
     function checkForm(password) {
         let passwordRegEx = /^[a-z0-9]+$/i;
         let passwordValue = password.value;
@@ -85,6 +191,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             alert('Password too short');
         }
     }
-</script>
+</script> -->
 
 </html>
