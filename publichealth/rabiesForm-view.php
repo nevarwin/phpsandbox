355f569c876e31b4a0c662d0a9d72c84df1dc454
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-25 22:43:20 +0800

    fix layout

--- publichealth/rabiesForm-view.php
+++ publichealth/rabiesForm-view.php
@@ -188,73 +188,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
             ?>
             <!-- <form action="" method="POST"> -->
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Type Of Exposure</label>
                 <div class="col-sm-6">
                     <p><?php echo $typeOfExposure; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Category</label>
                 <div class="col-sm-6">
                     <p><?php echo $category; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Bite Site</label>
                 <div class="col-sm-6">
                     <p><?php echo $biteSite; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Date Bitten</label>
                 <div class="col-sm-6">
                     <p><?php echo $dateBitten; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Type Of Animal</label>
                 <div class="col-sm-6">
                     <p><?php echo $typeOfAnimal; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Animal Status</label>
                 <div class="col-sm-6">
                     <p><?php echo $animalStatus; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Date Vacc Started</label>
                 <div class="col-sm-6">
                     <p><?php echo $dateVaccStarted; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Animal Vaccination</label>
                 <div class="col-sm-6">
                     <p><?php echo $animalVacc; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Wound Cleaned</label>
                 <div class="col-sm-6">
                     <p><?php echo $woundCleaned; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Rabies Vaccine</label>
                 <div class="col-sm-6">
                     <p><?php echo $rabiesVaccine; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Animal Outcome</label>
                 <div class="col-sm-6">
                     <p><?php echo $animalOutcome; ?></p>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-2">
                 <label for="" class='col-sm-3 col-form-label'>Case Class</label>
                 <div class="col-sm-6">
                     <p><?php echo $caseClass; ?></p>
