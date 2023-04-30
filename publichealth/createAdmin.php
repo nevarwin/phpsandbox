4950b4e972a1f4ff04effdd5270586fc2f33ed9f
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-30 20:09:19 +0800

    changed the alert button

--- publichealth/createAdmin.php
+++ publichealth/createAdmin.php
@@ -141,11 +141,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <?php
             if (!empty($errorMessage)) {
                 echo "
-            <div class='alert alert-warning alert-dismissible fade show' role='alert'>
-            <strong>$errorMessage</strong>
-            <button type = 'button' class = 'btn-close' data-bs-dismissible='alert' aria-label='Close'></button>
-            </div>
-            ";
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>Holy guacamole!</strong> $errorMessage
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+                ";
             }
             ?>
             <form action="" method="post">
@@ -216,13 +216,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <?php
                 if (!empty($successMessage)) {
                     echo "
-                <div class='row mb-3'>
-                <div class='offset-sm-3 col-sm-6'>
-                <div class='alert alert-success alert-dimissible fade show' role='alert'>
-                <strong>$successMessage</strong>
-                <button type = 'button' class = 'btn-close' data-bs-dismissible = 'alert' aria-label = 'close'></button>
-                </div>
-                </div>
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>Holy guacamole!</strong> $successMessage
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                 </div>
                 ";
                 }
@@ -235,9 +231,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <a href="/phpsandbox/publichealth/admin.php" class="btn btn-outline-primary" role="button">Cancel</a>
                     </div>
                 </div>
+            </form>
         </div>
-        </form>
-    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
